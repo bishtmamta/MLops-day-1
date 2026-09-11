81d1544c5ecaae54f456d5619975f6d0454400ba
user dd <bishtm2008@gmail.com>
2026-09-11 07:27:00 +0530

    fix model loading for ECS deployment

--- app.py
+++ app.py
@@ -1,35 +1,32 @@
-
 import streamlit as st
 import pandas as pd
-import mlflow
-import mlflow.sklearn
 import joblib
 import os
 
-import os
+# Get project root directory
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# Model path
 MODEL_PATH = os.path.join(BASE_DIR, "models", "champion_model.pkl")
+
 
 @st.cache_resource
 def load_model():
     return joblib.load(MODEL_PATH)
 
-# MLflow tracking URI
-mlflow.set_tracking_uri("sqlite:///mlflow.db")
 
-# Load model
-model = mlflow.sklearn.load_model(
-    "models:/Sales_Prediction_Model@champion"
-)
+# Load trained model
+model = load_model()
+
 
 # Streamlit app
-st.title("Advertising Sales Predictor")                                    
+st.title("Advertising Sales Predictor")
 
 # Input fields
 tv = st.number_input("TV Budget")
 radio = st.number_input("Radio Budget")
 newspaper = st.number_input("Newspaper Budget")
+
 
 # Prediction button
 if st.button("Predict"):
